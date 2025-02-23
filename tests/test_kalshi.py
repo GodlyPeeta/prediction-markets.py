5070751273c0ff974609cac0f491ba28d3b978f9
GodlyPeeta <peterzhou561@gmail.com>
2025-02-23 01:32:56 -0500

    refactor tests to use pytest instead of unittest

--- tests/test_kalshi.py
+++ tests/test_kalshi.py
@@ -1,23 +1,19 @@
 from __future__ import annotations
 
-import unittest
 from hypothesis import given, settings
 from hypothesis.strategies import integers 
 
 from prediction_markets import *
 
-class TestKalshi(unittest.TestCase):
+@given(integers(min_value=1, max_value=50))
+@settings(deadline=1000, max_examples=10)
+def test_client_market_read_and_load(num_markets):
+    kc = KalshiClient()
+    
+    markets = kc.get_markets(limit=num_markets)[0]
+    assert len(markets) == num_markets
 
-    # @given(integers(min_value=1, max_value=1000))
-    # @settings(deadline=1000, max_examples=1)
-    def test_client_market_read_and_load(self):
-        num_markets = 20
-        kc = KalshiClient()
-        
-        markets = kc.get_markets(limit=num_markets)[0]
-        self.assertEqual(len(markets), num_markets)
+    KalshiMarket.refresh_markets(markets)
 
-        KalshiMarket.refresh_markets(markets)
-
-        for m in markets:
-            self.assertIsNotNone(m.last_refreshed_data)
+    for m in markets:
+        assert m.last_refreshed_data is not None
